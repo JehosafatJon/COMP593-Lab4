dcfd8fa97ff9c3708dcb26e8a8e44a072d09ad3a
JehosafatJon <122817924+JehosafatJon@users.noreply.github.com>
2023-02-20 13:43:15 -0500

    Update Lab4
    - Added function header docstrings

--- lab4.py
+++ lab4.py
@@ -4,16 +4,17 @@ import re
 
 def main():
     
+    # Gets path of the log file
     log_file = get_log_file_path_from_cmd_line()
 
-    
+
     # Step 10
     dpt_tally = tally_port_traffic(log_file)
     for dpt, count in dpt_tally.items():
         if count > 100:
             generate_port_traffic_report(log_file, dpt)
             
-    
+
     # Step 11 Call
     generate_invalid_user_report(log_file)
 
@@ -25,6 +26,15 @@ def main():
 
 # Step 8
 def tally_port_traffic(log_file):
+    """Creates a tally of number of times a port was searched.
+
+    Args:
+        log_file (str): Path of the log file
+        
+    Returns:
+        (dict): Tallied totals for unique ports in the log file
+    """
+
     dest_port_logs = filter_log_by_regex(log_file, r' DPT=(.*?) ')[1]
 
     dpt_tally = {}
@@ -35,6 +45,14 @@ def tally_port_traffic(log_file):
 
 # Step 9
 def generate_port_traffic_report(log_file, port_number):
+    """ Writes data from the records of the specified port to a csv file
+
+    Args:
+        log_file (str): Path of the log file
+        port_number (str): The port number to generate the report on
+
+    """
+
     regex = r'^(.{6}) (.{8}) .*SRC=(.+?) DST=(.+?) .*SPT=(.+?) ' + f'DPT=({port_number})'
 
     captured_data = filter_log_by_regex(log_file, regex)[1]
@@ -47,6 +65,12 @@ def generate_port_traffic_report(log_file, port_number):
 
 # Step 11
 def generate_invalid_user_report(log_file):
+    """ Writes a report of invalid user records to a csv file.
+
+    Args:
+        log_file (str): Path of the log file
+    """
+
     regex = r'^(.{6}) (.{8}).*Invalid user (.*) from (.*)'
 
     captured_data = filter_log_by_regex(log_file, regex)[1]
@@ -58,6 +82,13 @@ def generate_invalid_user_report(log_file):
 
 # Step 12
 def generate_source_ip_log(log_file, ip_address):
+    """ Writes the records from a specified source ip to a log file.
+
+    Args:
+        log_file (str): Path of the log file
+        ip_address (str): The IP address to filter and generate the log for
+    """
+    
     regex = f'(.*SRC={ip_address}.*)'
     underscored_ip_address = re.sub(r"\.", "_", ip_address)
 
